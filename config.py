8b9e084ec42d653efdd39fcaebc8ad8f12776252
PhucHuwu <phuctranhuu37@gmail.com>
2025-02-05 13:23:17 +0700

    skibidi

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ customer_button_xpath = '//*[@id="a07FF5EDD-B3BD-450C-8023-76DB09ED5E81--5"]/div
 pay_button_xpath = "//sidebar-panel[@id='navigation.billing']//a[@title='Thanh toán']"
 verification_process_button_xpath = "//navigation-drawer-item//div[text()='Quy trình xác minh nhà quảng cáo']"
 start_verification_button_xpath = "/html/body/div[1]/root/div/div[1]/div[2]/div/div[3]/div/div/awsm-child-content/content-main/div/div[3]/identity-root/base-root/div/div[2]/div[1]/view-loader/advertiser-verification-view/div/div/converged-verification-not-started-card/div/div[2]/div[2]/material-button"
-save_and_continue_button_xpath = "/html/body/div[1]/root/div/div[1]/div[2]/div/div[3]/div/div/awsm-child-content/content-main/div/div[3]/identity-root/base-root/div/div[2]/div[1]/view-loader/wizard-orientation-view/div/div/questions-widget/div/div[3]/div[2]/div/material-button"
+save_and_continue_button_xpath = "//material-button[contains(@class, 'save-button') and .//div[contains(text(), 'Lưu và tiếp tục')]]"
 
 check_box_1_xpath = "(//material-ripple[@aria-hidden='true' and contains(@class, 'ripple')])[2]"
 check_box_2_xpath = "(//material-ripple[@aria-hidden='true' and contains(@class, 'ripple')])[4]"
